27b1301ab5911017e628873463e5e32d35e60cf8
skill-alba <167703984+skill-alba@users.noreply.github.com>
2024-05-31 22:29:29 +0300

    Update task2_backup.sh

--- task2_backup.sh
+++ task2_backup.sh
@@ -1,12 +1,13 @@
 #!bin/bash
-tar cpf /home/skill/HW7/archive/"HW_backup.tar" /home
+mkdir /archive
+tar cpf /archive/"HW_backup.tar" /home
 echo Home backup is done
-tar rf /home/skill/HW7/archive/"HW_backup.tar" /etc/vsftpd.conf
+tar rf /archive/"HW_backup.tar" /etc/vsftpd.conf
 echo FTP backup is done
-tar rf /home/skill/HW7/archive/"HW_backup.tar" /etc/ssh/sshd_config
+tar rf /archive/"HW_backup.tar" /etc/ssh/sshd_config
 echo SSH backup is done
-tar rf /home/skill/HW7/archive/"HW_backup.tar" /etc/xrdp
+tar rf /archive/"HW_backup.tar" /etc/xrdp
 echo XRDP backup is done
-tar rf /home/skill/HW7/archive/"HW_backup.tar" /var/log
+tar rf /archive/"HW_backup.tar" /var/log
 echo Var/log backup is done
 date "+%d-%m-%Y %H-%M-%S"
